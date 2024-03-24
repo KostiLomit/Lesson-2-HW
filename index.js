5846af82b3283dd9321500cbb0a46b51506f56f5
Klim D. <klimdudko@outlook.com>
2024-03-24 15:30:42 +0100

    5/9

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sum = x + y + z;
 
 console.log(sum);
 
-//2-st - exercise//
+//2-nd - exercise//
 const secInMin = 60;
 const minInHour = 60;
 const hourInDay = 24;
@@ -27,3 +27,40 @@ console.log("My age in day´s is " + myAgeInDays + " day´s");
 console.log("My age in hour´s is " + myAgeInhour + " hour´s");
 console.log("My age in min´s is " + myAgeInMin + " min´s");
 console.log("My age in second´s is " + myAgeInSec + " second´s");
+
+//3-rd - exercise//
+const count = 42;
+const userName = "42";
+
+//first option//
+const stringCount = String(count);
+const namberUserName = Number(userName);
+console.log(stringCount);
+console.log(namberUserName);
+//second option//
+const stringCount2 = count + "";
+const namberUserName2 = +userName;
+console.log(stringCount2);
+console.log(namberUserName2);
+
+//4 - exercise//
+const a = 1;
+const b = 2;
+const c = "белых медведей";
+
+const result = String(a) + String(b) + " " + c;
+ 
+console.log(result);
+
+//5 - exercise//
+let word1 = "доступ";
+let word2 = "морпех";
+let word3 = "наледь";
+let word4 = "попрек";
+let word5 = "рубило";
+
+let allWords = word1 + word2 + word3 + word4 + word5;
+
+let lengthWords = allWords.length;
+
+console.log(lengthWords);
